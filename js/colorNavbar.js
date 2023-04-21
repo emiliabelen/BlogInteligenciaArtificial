879b372b8eeaa7173346d9da3415badd2464ae32
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-04-21 09:39:25 -0300

    se terminó el header, donde esta hecho el cambio de color de tema y también se agregó el footer, se puso el icono favicon

---
+++ js/colorNavbar.js
@@ -0,0 +1,28 @@
+//leer en el localstorage si almacenamos un color de theme
+let temaConfigurado = JSON.parse(localStorage.getItem('tema')) || 'dark';
+let iconoTema = document.querySelector('#iconoTema');
+cambiarTema(temaConfigurado);
+
+let btnThemeDark = document.querySelector('#btnThemeDark');
+let btnThemeLight = document.querySelector('#btnThemeLight');
+
+btnThemeDark.addEventListener('click', () => cambiarTema('dark'));
+btnThemeLight.addEventListener('click', () => cambiarTema('light'));
+
+console.log(iconoTema);
+
+function cambiarTema(color) {
+  document.querySelector('html').setAttribute('data-bs-theme', color);
+  //guardar en localstorage
+  localStorage.setItem('tema', JSON.stringify(color));
+  //actualizar el icono
+  cambiarIcono(color);
+}
+
+function cambiarIcono(color) {
+  if (color === 'dark') {
+    iconoTema.classList.replace('bi-sun-fill', 'bi-moon-stars-fill');
+  } else {
+    iconoTema.classList.replace('bi-moon-stars-fill', 'bi-sun-fill');
+  }
+}
